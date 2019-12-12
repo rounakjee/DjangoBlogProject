0482707e8a336ccf638a2345fff9a33c73837cc0
Rounak <rounakchatterjee0797@gmail.com>
2019-12-13 00:49:17 +0530

    Django started ,templates created, login ,logout,profile with validation implemented

--- user/views.py
+++ user/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user/register.html', {'from': form})
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('Hello-Home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'user/profile.html')
 
